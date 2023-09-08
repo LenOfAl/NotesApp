8f13bd38ea5125e803768ae01ed439a63abcd684
allen <allennellasorry@gmail.com>
2023-09-08 22:39:50 +0530

    login and register view

---
+++ android/app/src/main/kotlin/com/theEndIsNear/comingsoon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.theEndIsNear.comingsoon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+// import 'package:comingsoon/views/login_view.dart';
+import 'package:comingsoon/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage()));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home ')),
+      body: FutureBuilder(
+        future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform,
+        ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              final user = FirebaseAuth.instance.currentUser;
+              if (user?.emailVerified ?? false) {
+                print('Verified user');
+              } else {
+                print('You are not verfied');
+              }
+              return const Text("Done");
+            default:
+              return const Text('Loading...');
+          }
+        },
+      ),
+    );
+  }
+}
